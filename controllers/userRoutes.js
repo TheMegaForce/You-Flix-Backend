01be0fc6126abdd7592c1b8d1874986550ce246a
Ishan Azad <ishansazad@gmail.com>
2023-10-04 17:29:47 -0700

    added more functionality to /register in userRoute

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -17,15 +17,33 @@ router.get('/', async (req, res) => {
 // Create: Add a new user (User Registration)
 router.post('/register', async (req, res) => {
   try {
-    // Validate and hash the password before creating the user
     const { name, image, username, password } = req.body;
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    
-    const newUser = await User.create({ name, image, username, password: hashedPassword });
-    res.status(201).json(newUser);
+
+    // Check if the username is already taken
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username is already taken.' });
+    }
+
+    const existingName = await User.findOne({ name });
+    if (existingName) {
+      return res.status(400).json({ message: 'The name { ' + name + ' } is already taken.' });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a new user
+    const newUser = new User({ name, image, username, password: hashedPassword });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).json({ message: 'User created successfully.' });
   } catch (error) {
-    res.status(400).json({ error: 'User registration failed' });
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error.' });
   }
 });
 
